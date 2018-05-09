2c8e800c8444abac77c46e1fc30d1e764865b1b4
kamila-ropela <kamila.ropela@gmail.com>
2018-05-09 18:10:13 +0200

    Notifications show once a day

--- app/src/main/java/com/example/bodzio/doctorapp/MainActivity.java
+++ app/src/main/java/com/example/bodzio/doctorapp/MainActivity.java
@@ -4,8 +4,10 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -18,6 +20,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(!prefs.getBoolean("firstTime", false)){
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.MINUTE, 00);
+            calendar.set(Calendar.HOUR_OF_DAY, 10);
+
+
+            Intent notifyIntent = new Intent(this, MyReceiver.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast
+                    (getApplicationContext(), 100, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            AlarmManager alarmManager = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                    86400000, pendingIntent);
+
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("firstTime", true);
+            editor.commit();
+        }
     }
 
     public void openCalendar(View view) {
@@ -28,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
     public void openAppointmentCreator(View view) {
         Intent intent = new Intent(this,  AppointmentCreator.class);
         startActivity(intent);
-        SetNotification();
     }
 
     public void addPatient(View view) {
@@ -45,19 +65,4 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,  ShowVisits.class);
         startActivity(intent);
     }
-
-    public void SetNotification() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.MINUTE, 10);
-        calendar.set(Calendar.HOUR_OF_DAY, 8);
-
-
-        Intent notifyIntent = new Intent(this, MyReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast
-                (getApplicationContext(), 100, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        AlarmManager alarmManager = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
-                86400000, pendingIntent);
-    }
-
 }
